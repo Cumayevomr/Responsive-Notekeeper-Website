2e82bfbd646f02fac9b5a72a47a86638d6541b5f
Cumayevomr <cumayevomr8@gmail.com>
2025-02-12 18:59:53 +0400

    javascript

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,17 @@
  */
 
 'use strict';
+
+/**
+  * Module import
+  */
+
+import { addEventOnElements } from "./utils.js";
+
+/**
+  * Toogle sidebar in small screen
+  */
+
+const /**{HTMLElement} */ $sidebar = document.querySelector('[data-sidebar]');
+const /**{Array<HTMLElement>} */ $sidebarTooglers = document.querySelector('[data-sidebar-toogler]');
+const /**{Array<HTMLElement>} */ $overlay = document.querySelector('[data-sidebar-overlay]');
--- src/js/utils.js
+++ src/js/utils.js
@@ -3,3 +3,21 @@
  */
 
 'use strict';
+
+/**
+ * Attaches  an event listener to a collaction of DOM elements.
+ * 
+ * @param {Array<HTMLElement>} $elements - An array of DOM elements to attach the event listeter to.
+ * @param {string} eventType - The type of event to listen to listen for (e.g., 'click', 'mouseover').
+ * @param {Function} callback - The function to be executed when the event occurs.
+ */
+
+const addEventOnElements = function ($elements, eventType, callback){
+    
+}
+
+
+
+export {
+    addEventOnElements
+}
