7b1089c2a8357ff17d9fca01be2c07f9c667c0ce
Cumayevomr <cumayevomr8@gmail.com>
2025-02-13 03:28:40 +0400

    client.js end

--- src/js/client.js
+++ src/js/client.js
@@ -5,8 +5,10 @@
 'use strict';
 
 import { NavItem } from "./components/NavItem";
+import { activeNotebook } from "./utils";
 
-const /** {HTMLElement} */ $sidebarList = document.querySelector('data-sidebar-lsit');
+const /** {HTMLElement} */ $sidebarList = document.querySelector('[data-sidebar-list]');
+const /** {HTMLElement} */ $notePanelTitle = document.querySelector('[data-note-panel-title]');
 
 /**
  * @namespace
@@ -26,6 +28,8 @@ export const client = {
         create(notebookData) {
             const /** {HTMLElement} */  $navItem = NavItem(notebookData.id, notebookData.name);
             $sidebarList.appendChild($navItem);
+            activeNotebook.call($navItem);
+            $notePanelTitle.textContent = notebookData.name;
         }
     }
 }
