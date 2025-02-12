02cc443dcbd6a17a72958d81faefe7c99f738954
Cumayevomr <cumayevomr8@gmail.com>
2025-02-13 03:08:40 +0400

    nav-item.js

--- src/js/client.js
+++ src/js/client.js
@@ -4,3 +4,23 @@
 
 'use strict';
 
+/**
+ * @namespace
+ * @property {Object} notebook
+ * @property {Object} note
+ *
+ */
+
+
+
+export const client = {
+
+    /**
+     * @parma {Object} notebookData
+     */
+    notebook: {
+        create(notebookData) {
+            consst /** {HTMLElement} */  $navItem = $navItem();
+        }
+    }
+}
--- src/js/components/NavItem.js
+++ src/js/components/NavItem.js
@@ -3,3 +3,15 @@
  */
 
 'use strict';
+
+/**
+ * @param {string} id
+ * @param {string} id
+ * @returns {HTMLElement}
+ */
+
+export const NavItem = function (id, name) {
+    const /** {HTMLElement} */ $navItem =document.createElement('div');
+    $navItem.classList.add('nav-item');
+    $navItem.setAttribute('data-notebook', id);
+}
